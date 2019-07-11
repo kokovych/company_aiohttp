8abc7cb62656ecd904ff5fc84d6d35fb311cd3df
Bohdan Kokovych <kokovych@gmail.com>
2019-07-12 00:43:39 +0300

    init db

--- common/db.py
+++ common/db.py
@@ -1,21 +1,34 @@
 # aiohttpdemo_polls/db.py
 from sqlalchemy import (
     MetaData, Table, Column, ForeignKey,
-    Integer, String, Date
+    Integer, String, Date,
 )
+from sqlalchemy.ext.declarative import declarative_base
+from passlib.hash import bcrypt
 
+
+Base = declarative_base()
 meta = MetaData()
 
-question = Table(
-    'user', meta,
 
-    Column('id', Integer, primary_key=True),
-    Column('username', String(200), nullable=False),
-    Column('password', String(200), nullable=False),
-    Column('email', String(256), nullable=False),
-    Column('first_name', String(256), nullable=True),
-    Column('last_name', String(256), nullable=True),
-    Column('registration_date', Date, nullable=False),
-    Column('role', Integer, nullable=False),
-)
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(150),)
+    email = Column(String(300), nullable=False, unique=True)
+    password = Column(String(300), nullable=False)
+    first_name = Column(String(300), nullable=False)
+    last_name = Column(String(300), nullable=False)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = bcrypt.encrypt(password)
+        self.email = email
+
+    def validate_password(self, password):
+        return bcrypt.verify(password, self.password)
+
+    def __repr__(self):
+        return "<User(username ='%s', password='%s', email='%s')>" % (self.username, self.password, self.email)
 
---
+++ init_db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, MetaData, Table
+
+from common.settings import config
+from common.db import Base
+
+
+DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
+
+def create_tables(engine):
+    meta = Base.metadata
+    meta.create_all(bind=engine)
+
+
+if __name__ == '__main__':
+    db_url = DSN.format(**config['postgres'])
+    engine = create_engine(db_url)
+    create_tables(engine)
